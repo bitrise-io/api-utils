9b262bf73f8b8a01640e0d83e1b2ebebb3a642be
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-14 14:50:33 +0200

    Add middleware test related stuffs

--- middleware/test.go
+++ middleware/test.go
@@ -1,10 +1,62 @@
 package middleware
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
 	"github.com/bitrise-io/api-utils/httpresponse"
+	"github.com/justinas/alice"
+	"github.com/stretchr/testify/require"
 )
+
+// TestCase ...
+type TestCase struct {
+	requestHeaders   map[string]string
+	expectedStatus   int
+	expectedResponse interface{}
+	middleware       alice.Chain
+}
+
+// PerformMiddlewareTest ...
+func PerformMiddlewareTest(t *testing.T,
+	httpMethod, url string,
+	tc TestCase,
+) {
+	t.Helper()
+
+	ts := httptest.NewServer(tc.middleware.Then(TestHandler()))
+	defer ts.Close()
+
+	var u bytes.Buffer
+	u.WriteString(string(ts.URL))
+	u.WriteString(url)
+
+	req, err := http.NewRequest(httpMethod, u.String(), nil)
+	require.NoError(t, err)
+
+	for key, val := range tc.requestHeaders {
+		req.Header.Add(key, val)
+	}
+	client := http.Client{}
+
+	res, err := client.Do(req)
+	require.NoError(t, err)
+
+	b, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+
+	require.Equal(t, tc.expectedStatus, res.StatusCode)
+	if tc.expectedResponse != nil {
+		expectedBytes, err := json.Marshal(tc.expectedResponse)
+		require.NoError(t, err)
+		require.Equal(t, string(expectedBytes), strings.Trim(string(b), "\n"))
+	}
+}
 
 // TestHandler ...
 func TestHandler() http.HandlerFunc {
