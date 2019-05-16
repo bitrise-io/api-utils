97b78c67f20a7892924db062739fa54931184fc6
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-16 08:40:58 +0200

    Split request params implementation to two files

--- providers/request_params.go
+++ providers/request_params.go
@@ -18,13 +18,3 @@ type RequestParams struct{}
 func (r *RequestParams) Get(req *http.Request) map[string]string {
 	return mux.Vars(req)
 }
-
-// RequestParamsMock ...
-type RequestParamsMock struct {
-	Params map[string]string
-}
-
-// Get ...
-func (r *RequestParamsMock) Get(req *http.Request) map[string]string {
-	return r.Params
-}
